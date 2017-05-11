e234568419024a03e298ff5a48e23343e21258f7
Jay Williams <jaytylerwilliams@gmail.com>
2017-05-11 15:44:12 -0400

    made some changes

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var config = require('./config');
+//var message = require('./routes/message');
+var urlencoded = require('body-parser').urlencoded;
+//var voice = require('./routes/voice');
+
+var app = express();
+app.use(urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+//app.post('/api/sms', message);
+//app.post('/api/call', voice)
+
+module.exports = app;
